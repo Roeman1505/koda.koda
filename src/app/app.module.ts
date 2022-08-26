0d7fd649d29f5bcf49d6be0b758e5e58f8bf514a
Roeman1505 <robinsonemmanuel1505@gmail.com>
2022-08-26 10:37:44 +0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BackgroundComponent } from './background/background.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material,module';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { MembersComponent } from './members/members.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LogoComponent } from './logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BackgroundComponent,
+    MenuComponent,
+    AboutComponent,
+    MembersComponent,
+    FooterComponent,
+    LogoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule,
+    FontAwesomeModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/members/members.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+members =[
+  {name:'Ngoswe', imageone:'/assets/images/zenji.jpg', imagetwo:'/assets/images/laptop.jpg',description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the  galley of type and scrambled it to make a type specimen book."},
+
+  {name:'Eng.Msengi', imageone:'/assets/images/zanzi.jpg', imagetwo:'/assets/images/laptop1.jpg',description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the  galley of type and scrambled it to make a type specimen book."},
+  {name:'Debian', imageone:'/assets/images/ngoro.jpg', imagetwo:'/assets/images/laptop2.jpg',description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the  galley of type and scrambled it to make a type specimen book."},
+  {name:'Roeman', imageone:'/assets/images/kili.jpg', imagetwo:'/assets/images/laptop3.jpg',description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the  galley of type and scrambled it to make a type specimen book."},
+ 
+]
+
+}
